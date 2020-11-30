de60ef0631f32989d3ab888ca92724d986fce26d
Sarah Hart Landolt <sarahhartlandolt@sarahs-mbp.attlocal.net>
2020-11-30 16:32:11 -0600

    added a few functions

--- modern-farm/src/scripts/harvester.js
+++ modern-farm/src/scripts/harvester.js
@@ -6,14 +6,25 @@ const isCorn = (plant) => {
 
 export const harvestPlants = (plantsArray) => {
     const outputArray = plantsArray.flatMap(plant => new Array(isCorn(plant)).fill(plant));
-    //  anothey alternative: 
+    //  another alternative: 
+
     // const number= plant.type === "Corn" ? plant.output * .5 : plant.output;
     // for (const plant of plantsArray) {
     //     let newArray = new Array(number).fill(plant);
+    //     console.log(newArray)
     //     outputArray.push(...newArray);
     // }
-
-    console.log(outputArray);
-    return outputArray;
+  
+    // another alternative :
+    // for(let i=0; i < outputArray.length; i++){
+    //     outputArray[i]= {...outputArray[i], id: i+1}
+    // }
+    const outputWithId = outputArray.map((plant, index)=>({...plant, id: index + 1}))
+    return outputWithId;
 
 }
+
+
+
+
+
